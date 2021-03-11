eb9bbce75dae68da1a4f058ffc746ecad2a42d4f
Reinhard Herzog <reinhard.herzog@iosb.fraunhofer.de>
2021-03-11 18:05:58 +0100

    fix version

--- Controller.bat
+++ Controller.bat
@@ -4,5 +4,5 @@ set BASE_URL=http://localhost:8080/FROST-Server/v1.0/
 REM Benchmark Session Identifier within Benchmark thing to be used
 set SESSION=0001
 
-java -jar .\BenchmarkController\target\BenchmarkController-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar .\BenchmarkController\target\BenchmarkController-1.1-SNAPSHOT-jar-with-dependencies.jar
 pause
